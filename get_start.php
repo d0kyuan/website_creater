554bdb0436eba23e535d48b6110a2524e343fd01
d0kyuan <rayyuan5010@gmail.com>
2017-08-15 21:44:04 +0800

    change style

--- get_start.php
+++ get_start.php
@@ -7,26 +7,40 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
-<body>
-    <div class="head">第一步</div>
-    <div class="content">
+
+<body style="width:50%;margin:5% auto;">
+    <h1 class="head">第一步</h1>
+    <hr>
+    <br>
+    <div class="content" >
         <form action="asset/prog/web_setting.php" method="post">
-            <select name="name">
-                <?php
-                    require_once('asset/prog/db_conf.php');
-                    
-                    foreach($db->query('select Conf_Name,Conf_Id from tb_config ') as $row){
-                        print('<option value="'.$row['Conf_Name'].'">'.$row['Conf_Name'].'</option>');
-                    }
-                ?>
-            </select>
-            <input name="ac">
+            <label>
+                選擇您的網站:
+                 <select name="name" required>
+                     <option value="-1">請選擇</option>
+                    <?php
+                        require_once('asset/prog/db_conf.php');
+
+                        foreach($db->query('select Conf_Name,Conf_Id from tb_config ') as $row){
+                            print('<option value="'.$row['Conf_Name'].'">'.$row['Conf_Name'].'</option>');
+                        }
+                    ?>
+                </select>
+            </label>
             <br>
             <br>
-            <input name="pwd">
+            <br>
+            <label>帳號:<input name="ac" required></label>
+            <br>
+            <br>
+            <br>
+            <label>密碼:<input type="password" name="pwd" required></label>
+            <br>
+            <br>
             <br>
             <br>
             <button>管理網站</button>
+
         </form>
         <br>
         <br>
